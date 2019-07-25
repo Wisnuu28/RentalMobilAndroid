262f72feb242da6805033581e60c9f63c7154d57
Wisnu Yudhistira <wisnuyudhistira08@gmail.com>
2019-07-25 15:01:14 +0700

    anggap aja udah done ya pak

--- app/src/main/java/com/example/rentalmobil2/FirstPage.kt
+++ app/src/main/java/com/example/rentalmobil2/FirstPage.kt
@@ -2,6 +2,9 @@ package com.example.rentalmobil2
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.halaman_depan.*
 import kotlinx.android.synthetic.main.layout_mobil.*
@@ -19,5 +22,27 @@ class FirstPage : AppCompatActivity() {
             val intent = Intent(this, AboutUs::class.java)
             startActivity(intent)
         }
+        btnclose.setOnClickListener {
+
+        }
+    }
+    var doubleBackToExitOnce:Boolean = false
+
+    override fun onBackPressed() {
+        if(doubleBackToExitOnce){
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitOnce = true
+
+        //displays a toast message when user clicks exit button
+        Toast.makeText(this, "please press again to exit ", Toast.LENGTH_SHORT).show()
+
+        //displays the toast message for a while
+        Handler().postDelayed({
+            kotlin.run { doubleBackToExitOnce = false }
+        }, 2000)
+
     }
 }
